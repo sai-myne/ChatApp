d5519d84b9257c93ea83d1b5e7f7c704637e7672
sai-myne <ahlusir@gmail.com>
2021-07-13 19:45:49 +0800

    User Registration & Validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,72 @@
-const { User } = require('../models')
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
 
 module.exports = {
-    Query: {
-      getUsers: async () => {
-          try{
-              const users = await User.findAll()
+  Query: {
+    getUsers: async () => {
+      try {
+        const users = await User.findAll();
 
-              return users
-          } catch(err){
-              console.log(err)
-          }
-      },
-    }
-  };
+        return users;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  Mutation: {
+    register: async (_, args) => {
+      let { username, email, password, confirmPassword } = args;
+      let errors = {};
+
+      try {
+        // Validate input data
+        if (email.trim() === "") errors.email = "Email must not be empty";
+        if (username.trim() === "")
+          errors.username = "Username must not be empty";
+        if (password.trim() === "")
+          errors.password = "Password must not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "Repeat password must not be empty";
+
+        if (password !== confirmPassword)
+          errors.confirmPassword = "passwords must match";
+        // Check if username / email exists
+        // const userByUsername = await User.findOne({ where: { username }})
+        // const userByEmail = await User.findOne({ where: { email }})
+
+        // if(userByUsername) errors.username = 'Username is taken'
+        // if(userByEmail) errors.email = 'Email is taken'
+
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
+
+        // hash password
+        password = await bcrypt.hash(password, 6);
+
+        // Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+
+        // Return user
+        return user;
+      } catch (err) {
+        console.log(err);
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} is already taken`)
+          );
+        } else if (err.name === "SequelizeValidationError"){
+            err.errors.forEach(
+                e => errors[e.path] = e.message
+            )
+        }
+        throw new UserInputError("Bad input", { errors });
+      }
+    },
+  },
+};
