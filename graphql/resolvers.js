31acbc55e552ff0be8a1046ec33eefc3259dc069
sai-myne <ahlusir@gmail.com>
2021-07-14 18:38:42 +0800

    Login & Generating JWT

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,78 @@
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
-const { UserInputError } = require("apollo-server");
+const { UserInputError, AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/env.json");
+const { Op } = require('sequelize')
 
 module.exports = {
   Query: {
-    getUsers: async () => {
+    getUsers: async (_, __, context) => {
       try {
-        const users = await User.findAll();
+        if (context.req && context.req.headers.authorization) {
+          const token = context.req.headers.authorization.split("Bearer ")[1];
+          jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+              throw new AuthenticationError("Unauthenticated");
+            }
+            user = decodedToken;
+
+          });
+        }
+        const users = await User.findAll({
+            where: { username: { [Op.ne]: user.username }}
+        });
 
         return users;
       } catch (err) {
         console.log(err);
+        throw err
+      }
+    },
+    login: async (_, args) => {
+      const { username, password } = args;
+      let errors = {};
+      try {
+        if (username.trim() === "")
+          errors.username = "username must not be empty";
+        if (password.trim() === "")
+          errors.password = "password must not be empty";
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError("bad input", { errors });
+        }
+        const user = await User.findOne({
+          where: { username },
+        });
+        if (!user) {
+          errors.username = "user not found";
+        }
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError("user not found", { errors });
+        }
+        const correctPassword = await bcrypt.compare(password, user.password);
+
+        if (!correctPassword) {
+          errors.password = "password is incorrect";
+          throw new AuthenticationError("password is incorrect", { errors });
+        }
+
+        const token = jwt.sign(
+          {
+            username,
+          },
+          JWT_SECRET,
+          { expiresIn: 60 * 60 }
+        );
+        return {
+          ...user.toJSON(),
+          createdAt: user.createdAt.toISOString(),
+          token,
+        };
+      } catch (err) {
+        console.log(err);
+        throw err;
       }
     },
   },
@@ -60,10 +122,8 @@ module.exports = {
           err.errors.forEach(
             (e) => (errors[e.path] = `${e.path} is already taken`)
           );
-        } else if (err.name === "SequelizeValidationError"){
-            err.errors.forEach(
-                e => errors[e.path] = e.message
-            )
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message));
         }
         throw new UserInputError("Bad input", { errors });
       }
