acd606f3f88aa2b74ae50e99033e90e008643d76
sai-myne <ahlusir@gmail.com>
2021-07-15 19:55:06 +0800

    Writing Register & Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import "./App.scss";
 import { Container } from 'react-bootstrap';
 import Register from "./pages/Register";
+import ApolloProvider from "./ApolloProvider";
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   
   return (
-    <Container className="pt-5">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+          
+        </Container>
+      </BrowserRouter>
+      
+    </ApolloProvider>
+    
   );
 }
 
