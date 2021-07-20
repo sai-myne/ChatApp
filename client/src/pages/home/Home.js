29f44d13679a3841f1d6d2683134c642d7696833
sai-myne <ahlusir@gmail.com>
2021-07-20 19:40:33 +0800

    Messages Context & Styling

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+import { Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import { useAuthDispatch } from "../../context/auth";
+import Users from "./Users";
+import Messages from "./Messages";
+
+export default function Home({ history }) {
+  const dispatch = useAuthDispatch();
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
+
+  return (
+    <Fragment>
+      <Row className="bg-white justify-content-around mb-1">
+        <Link to="/login" style={{ width: "0px" }}>
+          <Button variant="link">Login</Button>
+        </Link>
+        <Link to="/register" style={{ width: "0px" }}>
+          <Button variant="link">Register</Button>
+        </Link>
+        <Button variant="link" onClick={logout} style={{ width: "0px" }}>
+          Logout
+        </Button>
+      </Row>
+      <Row className="bg-white">
+        <Users />
+        <Messages />
+      </Row>
+    </Fragment>
+  );
+}
