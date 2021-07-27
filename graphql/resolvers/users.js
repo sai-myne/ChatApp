332c97685592fab6a5afd72fbdeeedabf68be8d9
sai-myne <ahlusir@gmail.com>
2021-07-27 18:55:17 +0800

    Reactions Mutation & Subscription

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -75,7 +75,6 @@ module.exports = {
         );
         return {
           ...user.toJSON(),
-          createdAt: user.createdAt.toISOString(),
           token,
         };
       } catch (err) {
